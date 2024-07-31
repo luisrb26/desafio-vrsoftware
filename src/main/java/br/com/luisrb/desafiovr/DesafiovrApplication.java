1fd4dde256f5adcf2da1b028623f451de5eda411
Luís Silva <gabrielima.si@gmail.com>
2024-07-31 14:13:40 -0300

    Início de projeto Spring

---
+++ src/main/java/br/com/luisrb/desafiovr/DesafiovrApplication.java
@@ -0,0 +1,13 @@
+package br.com.luisrb.desafiovr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiovrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafiovrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/luisrb/desafiovr/DesafiovrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luisrb.desafiovr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiovrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
